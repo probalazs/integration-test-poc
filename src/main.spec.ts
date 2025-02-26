7695a2820e7d8b042cfe135e9b33856af5c0feda
Balazs Prokics <balazs.prokics@fizz.hu>
2025-02-26 12:51:35 +0100

    remove main

--- src/main.spec.ts
+++
@@ -1,9 +0,0 @@
-import { main } from "./main";
-
-describe("Main", () => {
-  it("should return 'Hello World'", () => {
-    const result = main();
-
-    expect(result).toBe("Hello World");
-  });
-});
--- src/main.ts
+++
@@ -1,3 +0,0 @@
-export type Main = () => string;
-
-export const main: Main = () => "Hello World";

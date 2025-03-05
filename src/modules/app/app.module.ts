36bd9426262bbe7c27401295a50f83f0a63cc132
Balazs Prokics <balazs.prokics@fizz.hu>
2025-03-05 16:21:12 +0100

    use common module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './services/app.service';
+import { CommonModule } from '../common/common.module';
+import { config } from '../../config';
+import { entities } from '../../entities';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const connection: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: config.pgHost,
+  port: config.pgPort,
+  username: config.pgUser,
+  password: config.pgPassword,
+  database: config.pgDb,
+  entities,
+};
 
 @Module({
-  imports: [],
+  imports: [CommonModule.forRoot(connection)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
